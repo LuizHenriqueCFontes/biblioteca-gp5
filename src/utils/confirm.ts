8ddef2a1d2dba6180ff0362953092c3d20cad72f
Luiz <luizhenrique3781@gmail.com>
2026-06-04 18:53:21 -0300

    feat: add utils confirm

---
+++ src/utils/confirm.ts
@@ -0,0 +1,16 @@
+
+import Swal from "sweetalert2";
+
+export async function confirmAction(title: string, text: string):Promise<boolean> {
+
+    const result = await Swal.fire({
+        title,
+        text,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Confirmar",
+        cancelButtonText: "Cancelar"
+    });
+
+    return result.isConfirmed
+}
