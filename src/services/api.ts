0de11d55f468b4541640049a12527714813bc986
Luiz <luizhenrique3781@gmail.com>
2026-06-18 21:24:23 -0300

    feat: initialize react reader

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
 
-    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGktYmlibGlvdGVjYS1ncDUiLCJzdWIiOiI4Yzk3MTVhOS02NWM3LTQ2OTEtYTcxNi1hMTQ5N2NjYzhiMDIiLCJyb2xlIjpbIkFETUlOIl0sImV4cCI6MTc4MTgwMzY4Mn0.Y7sx9aT5Q78MkCDYBFiFbDDGRjWaABIGiHIZWrw4K40");
+    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGktYmlibGlvdGVjYS1ncDUiLCJzdWIiOiI0YWEwNWZjOC1lZDNiLTQ5YTEtOGMwMS04ZDk1MmI2ZDEyZTIiLCJyb2xlIjpbIlVTRVIiXSwiZXhwIjoxNzgxODMwNzIwfQ.lkpxAlBo4SVLK-nZVR8S6EtHk3ATxZyLGsAJTIcJ_lY");
 
     const token = localStorage.getItem("token");
 
