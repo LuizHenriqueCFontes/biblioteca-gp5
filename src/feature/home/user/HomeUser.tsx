55e036cdbc99066962b8f124be3b4e0f6c26f71f
Luiz <luizhenrique3781@gmail.com>
2026-07-15 20:05:35 -0300

    feat: initialize styles to home use

--- src/feature/home/user/HomeUser.tsx
+++ src/feature/home/user/HomeUser.tsx
@@ -4,6 +4,7 @@ import { Button } from "../../../shared/components/Button/Button";
 import { useSearchBooks } from "../../books/user/searchBooks/hooks/useSearchBooks";
 import BookCard from "../../books/components/BookCard/BookCard";
 import styles from "./HomeUser.module.css";
+import homeUseBackground from "../../../assets/images/home/home-user-background.png";
 
 export default function HomeUser() {
 
@@ -12,26 +13,28 @@ export default function HomeUser() {
     const { books, loadingBooks, totalElements } = useSearchBooks(undefined, undefined, {page: 0, size: 6});
 
     return(
-        <section>
+        <section className={styles.container}>
             <div className={styles.titleContainer}>
-                <div>
-                    <span>Bem-vindo à sua biblioteca</span>
-                    <h1>Conhecimento que transforma</h1>
-                    <p>Explore milhares de livros gratuitos, organize suas leituras e descubra novos mundos todos os dias.</p>
-
-                    <Button type="button" variant="primary" icon={BookOpen}>Explorar livros</Button>
-
-                    <Button type="button" variant="secondary" icon={Bookmark}>Meus emprestimos</Button>
+                <div className={styles.containerContent}>
+                    <span className={styles.tagTitle}>Bem-vindo à sua biblioteca</span>
+                    <h1 className={styles.title}>Conhecimento que transforma</h1>
+                    <p className={styles.description}>Explore milhares de livros gratuitos, organize suas leituras e descubra novos mundos todos os dias.</p>
+                    <div>
+                        <Button className={styles.btn} type="button" variant="primary" icon={BookOpen}>Explorar livros</Button>
+                        <Button className={styles.btn} type="button" variant="secondary" icon={Bookmark}>Meus emprestimos</Button>
+                    </div>
                 </div>
+
+                <img src={homeUseBackground} alt="Imagem de fundo" className={styles.bannerImage}/>
             </div>
 
             <article>
-                <h3>Categorias populares</h3>
+                <h3>Categorias</h3>
 
                 {loadingListCategories ?? <p>Carregando...</p>}
 
                 {listCategories.map((categorie) => (
-                    <div>
+                    <div key={categorie.idCategory}>
                        <Bookmark />
                        <span>{categorie.name}</span> 
                     </div>
@@ -39,7 +42,7 @@ export default function HomeUser() {
             </article>
 
             <article>
-                <h3>Recomendados para você</h3>
+                <h3>Livros sugeridos</h3>
 
                 {loadingBooks ?? <p>Carregando...</p>}
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
 
-    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGktYmlibGlvdGVjYS1ncDUiLCJzdWIiOiI4Yzk3MTVhOS02NWM3LTQ2OTEtYTcxNi1hMTQ5N2NjYzhiMDIiLCJyb2xlIjpbIkFETUlOIl0sImV4cCI6MTc4NDEyODI3NH0.zJnu174-BrOwu6YO3Z3qe8dqexBLQjVfPXcdhY30p_o");
+    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGktYmlibGlvdGVjYS1ncDUiLCJzdWIiOiIwMDUxYWU5Ni01M2JiLTQ0NTItODJkZC02YjA2YTY0ZTU0OTEiLCJyb2xlIjpbIkFETUlOIl0sImV4cCI6MTc4NDE1ODU5NH0.52kT_Hw4V8UdQlWLIfPndZBU-Y-lcop2Eoz6ZG6Gt3Q");
 
     const token = localStorage.getItem("token");
 
