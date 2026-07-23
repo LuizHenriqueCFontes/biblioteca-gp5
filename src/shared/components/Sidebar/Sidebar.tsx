3cb401198d66effa659b79f0112c166a8a8f0e90
Luiz <luizhenrique3781@gmail.com>
2026-07-22 21:32:22 -0300

    feat: add radix collapsible

--- src/shared/components/Sidebar/Sidebar.tsx
+++ src/shared/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { AnimatePresence, motion } from "framer-motion";
 import styles from "./Sidebar.module.css";
 import * as Dialog from "@radix-ui/react-dialog";
-import { Book, Home, X } from "lucide-react";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { Book, BookUp, Home, User, Users, X } from "lucide-react";
 import { Link } from "react-router-dom";
 import { authStorage } from "../../../feature/auth/services/authStorage";
 
@@ -11,6 +12,7 @@ interface Sidebar {
 }
 
 const role = authStorage.getRole();
+const isAdmin = role === "ADMIN";
 
 export default function Sidebar(props: Sidebar){
     return(
@@ -32,15 +34,56 @@ export default function Sidebar(props: Sidebar){
                             exit={{ x: "-100%" }}
                             transition={{duration: 0.25}}>
                                 
-                                <button> <X /> </button>
+                                <button onClick={() => props.onOpenChange(false)}> <X /> </button>
 
                                 <nav>
                                     <ul>
                                         <li>
-                                            <Link to={role === "ADMIN" ? "/admin/home" : "/"}><Home /> {role === "ADMIN" ? "Dashboard" : "Tela de início"}</Link>
+                                            <Link to={isAdmin ? "/admin/home" : "/"}><Home /> {isAdmin ? "Dashboard" : "Tela de início"}</Link>
                                         </li>
                                         
-                                        <li><Link to={role === "ADMIN" ? "/admin/imports" : "/search/books"}><Book /> {role === "ADMIN" ? "Importar livros" : "Pesquisar livros"}</Link></li>
+                                        <li>
+                                            <Link to={isAdmin ? "/admin/imports" : "/search/books"}><Book /> {isAdmin ? "Importar livros" : "Pesquisar livros"}</Link>
+                                        </li>
+
+                                        <li>
+                                            <Link to={isAdmin ? "/admin/list/users" : "/loan"}>{isAdmin ? 
+                                            <>
+                                                <Users /> 
+                                                <span>Listar usuários</span>
+                                            </> 
+                                            :
+                                            <>
+                                                <BookUp /> 
+                                                <span>Meus empréstimos</span>
+                                            </>}</Link>
+                                        </li>
+
+                                        {
+                                            isAdmin && 
+                                            <li>
+                                                <Link to={"/categories"}>Categorias</Link>
+                                            </li>
+                                        }
+
+                                        <li>
+                                            <Collapsible.Root>
+
+                                                <Collapsible.Trigger>
+                                                    <User /> Meu Perfil
+                                                </Collapsible.Trigger>
+
+                                                <Collapsible.Content asChild>
+                                                    <ul>
+                                                        <li>Minha conta</li>
+                                                        <li>Editar dados</li>
+                                                        <li>Sair</li>
+                                                    </ul>
+
+                                                </Collapsible.Content>
+
+                                            </Collapsible.Root>
+                                        </li>
                                     </ul>
                                 </nav>
 
